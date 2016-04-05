2d4ffa9175742785317544df1d9f9f085c0437a0
Michael Moyles <micmoyles@gmail.com>
2016-04-05 21:49:19 +0100

    update form and animate menu button

--- bootstrap/app.js
+++ bootstrap/app.js
@@ -3,5 +3,9 @@ var main = function() {
 	$('.btn-info').click(function() {
 	$('.sql').toggle();	
 });
+
+	$('.btn-menu').click(function() {
+	$('.menu-items').toggle();	
+});
 }
 $(document).ready(main);
