c982571158f0cd6b3d2a2197a2b135155b75a985
Michael Moyles <micmoyles@gmail.com>
2018-08-30 12:10:49 +0100

    shorten logic, still need to ensure only unique elements are added

--- hackerrank/nonDivisibleSubset.py
+++ hackerrank/nonDivisibleSubset.py
@@ -20,25 +20,41 @@ def getPermutations(k, S):
 
 def getSubSets(s):
 
-    startingPoint = 0
+    # this currently does not return a unique list or subsets
 
     # largest subset
-    max = len(s) - 1
+
+    max = len(s)
+    size = 2 # size starts at 2 because we do not need to loop over 1 element subsets
 
     # first add all single elements
-    retVal = [[x] for x in s]
+    retVal = [{x} for x in s]
 
     # next step to add all subsets with 2<=size<len(s)
-    while startingPoint < max:
-        for i in xrange(1,len(s)+1):
-            substr = s[startingPoint:i]
-            if substr:
-                retVal.append(substr)
-        startingPoint+=1
+    # get all two element subsets
+    # take each two element subset and add one element to get the three element subsets
+    # repeat
 
-    retVal.append([s[max]])
+    while size < max:
+
+        for r in [x for x in retVal if len(x) == size - 1 ]:
+            print 'Looking at '
+            print r
+            # ie get two element arrays when we are creating 3 element arrays
+            for i in xrange(len(s)):
+                print 'Element ' + str(s[i])
+                if s[i] not in r:
+                    f = r
+                    print 'Adding ' + str(s[i]) + ' to '
+                    print r
+                    f.add(s[i])
+                    retVal.append( f )
+                    print retVal
+
+        size = max
+
     return retVal
 
-S = [1,5,9,8]
+S = [1,5,9,8,7]
 k = 5
 print getSubSets(S)
