9f1ad1389367ee9c1a7f1403429480192f77dccf
Michael Moyles <micmoyles@gmail.com>
2018-09-28 11:39:38 +0100

    bubble sort - make tidier to use for imports

--- hackerrank/bubbleSort.py
+++ hackerrank/bubbleSort.py
@@ -1,5 +1,7 @@
-def countSwaps(a):
-    i = j = count = 0
+#!/usr/bin/python
+
+def countSwaps(a, showInfo = False):
+    count = 0
     while not checkSort(a):
         for j in xrange(0,len(a)-1):
             if a[j] > a[j+1]:
@@ -11,11 +13,18 @@ def countSwaps(a):
                 larger  = a[j]
                 a[j]    = smaller
                 a[j+1]  = larger
-    print('Array is sorted in %d swaps.' % count)
-    print('First Element: %d' % a[0])
-    print('Last Element: %d' % a[len(a)-1])
+    if showInfo:
+        print('Array is sorted in %d swaps.' % count)
+        print('First Element: %d' % a[0])
+        print('Last Element: %d' % a[len(a)-1])
+
+    return a
+
 def checkSort(a):
     for i in xrange(len(a)-1):
         if a[i] > a[i+1]:
             return False
     return True
+
+def myBubbleSort(a):
+    return countSwaps(a, showInfo=False)
