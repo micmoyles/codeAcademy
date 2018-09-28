98979a1cc7e270997f46a8b1c728609e61989f5d
Michael Moyles <micmoyles@gmail.com>
2018-09-28 15:37:34 +0100

    apply correct use of arguments for array insert method :(

--- hackerrank/fraudulant.py
+++ hackerrank/fraudulant.py
@@ -6,8 +6,10 @@ https://www.hackerrank.com/challenges/fraudulent-activity-notifications/problem?
 from bubbleSort import myBubbleSort
 import math
 
-def getMedian(l):
-    l = myBubbleSort(l)
+def getMedian(l, needToSort = False):
+    if needToSort:
+        l = myBubbleSort(l)
+
     # if length of l is even we take the average of the middle two numbers
     if len(l) % 2 == 0:
         middle1 = len(l) / 2
@@ -16,7 +18,25 @@ def getMedian(l):
     else:
         middle = int(math.floor( len(l) / 2.0 ))
         median = l[middle]
+
     return median
+
+def insertInOrder(a,l):
+    print 'Inserting ' + str(l) + ' into '+str(a)
+    if l >= a[len(a)-1]:
+        a.append(l)
+        return a
+    elif l <= a[0]:
+        a.insert(0,l)
+        return a
+
+    for i in xrange(0,len(a)):
+        if l == a[i]:
+            a.insert(i,l)
+            return a
+        elif l > a[i] and l < a[i+1]:
+            a.insert(i+1,l)
+            return a
 
 def activityNotifications(expenditure, d):
     '''
@@ -24,16 +44,36 @@ def activityNotifications(expenditure, d):
     d: an integer, the lookback days for median spending
     '''
     print expenditure
-    for l in xrange(d,len(expenditure)):
+
+    alerts = 0
+    l = d
+    curExp = expenditure[l - d:l]
+    curExp = sorted(curExp)
+
+    while l < len(expenditure):
+    #for l in xrange(d,len(expenditure)):
         # in array [a b c d e f] we want to choose e and get the median of [a b c d]
-        curExp = expenditure[0:l]
+        #curExp = expenditure[l-d:l]
 
         today = expenditure[l]
         print today, curExp, getMedian(curExp)
-        if today >= getMedian(curExp): print 'Alert Issued'
+        if today >= 2 * getMedian(curExp):
+            alerts+=1
+            print 'Alert Issued'
+
+        # remove first element
+        #print 'Butchering'
+        #print curExp
+        curExp.pop(0)
+        #print curExp
+        # insert next element into the already sorted list
+        curExp = insertInOrder(curExp,expenditure[l])
+        #print curExp
+        l+=1
 
 p = [5,8,9,6,4,7,7,55,7,8]
 e = [2, 3, 4, 2, 3, 6, 8, 4, 5]
+f = [10, 20 , 30 ,40, 50]
 activityNotifications(e,5)
 
 
